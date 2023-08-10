1cb2b2d4129ed031896f995fd234de7677011ac3
tricornerlab <138512353+tricornerlab@users.noreply.github.com>
2023-08-10 14:54:12 +0200

    Update main.blade.php

--- resources/views/layouts/default/main.blade.php
+++ resources/views/layouts/default/main.blade.php
@@ -4,7 +4,7 @@
     {{--    BANNER--}}
 
     <section class="max-w-6xl  m-auto px-auto   bg-white  position-static mt-2">
-        <img src="/app/ganhbd/public/img/slide-5.jpeg" class="w-[100%] h-auto pt-5 parent position-sticky">
+        <img src="{{ env('APP_URL')}}/img/slide-5.jpeg" class="w-[100%] h-auto pt-5 parent position-sticky">
         <div class="position-relative sopra0 font3 text-md">
             <p class=" h-[40px] w-fit bg-[#6091ba] text-white px-3 py-1.5  uppercase mb-1">
                 {{ __('sections.banner1') }}
