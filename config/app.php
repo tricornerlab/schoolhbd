cb79cacc73c926d4f8bb78bf7541b0f3ddc5e213
tricornerlab <138512353+tricornerlab@users.noreply.github.com>
2023-08-11 14:32:44 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Ecole Ohalei Menahem'),
+    'name' => env('APP_NAME', 'Ecole'),
 
     /*
     |--------------------------------------------------------------------------
