39016454bf03e5038d71a7fcaedfcfa6ffe0b0fc
tricornerlab <138512353+tricornerlab@users.noreply.github.com>
2023-08-11 19:07:40 +0200

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -51,7 +51,7 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
+            'ignore_exceptions' => true,
         ],
 
         'single' => [
