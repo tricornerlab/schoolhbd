a91c22cf240606f8c78eb457766d94aa2ce5ad06
tricornerlab <138512353+tricornerlab@users.noreply.github.com>
2023-08-23 16:26:28 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
 
         <script src="https://cdn.tailwindcss.com"></script>
-        <link rel="stylesheet" href="{{ env('APP_URL').'/css/app.css' }}">
+<!--         <link rel="stylesheet" href="{{ env('APP_URL').'/css/app.css' }}"> -->
 
         <script src="https://cdn.tailwindcss.com"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
