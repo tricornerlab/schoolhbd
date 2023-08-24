b06f7e3c39bc51c022961a82eedc32602e714562
tricornerlab <138512353+tricornerlab@users.noreply.github.com>
2023-08-24 11:55:01 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'schoolhbd2'),
-            'username' => env('DB_USERNAME', 'admin1'),
+            'username' => env('DB_USERNAME', 'admin3'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
